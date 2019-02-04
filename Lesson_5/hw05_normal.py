5d852527f1be78490583f17af2adde10b9eb34cd
artmax17 <artmax17@mail.ru>
2019-02-05 00:30:19 +0300

    Решение hw05_normal, добавлены функции в hw05_easy

--- Lesson_5/hw05_normal.py
+++ Lesson_5/hw05_normal.py
@@ -15,15 +15,36 @@
 # и импортированные в данный файл из easy.py
 # ИСПОЛЬЗОВАТЬ МОДУЛЬ OS и SHUTIL
 
+import os
+import sys
+import hw05_easy
 
+user_choice = 'b'
+while user_choice != '0':
+    print('Press 1 for change directory')
+    print('Press 2 for explore current directory')
+    print('Press 3 for remove directory')
+    print('Press 4 for create directory')
+    print('Press q for q1uit')
+    user_choice = input()
+    print(user_choice)
 
+    if user_choice == 'q':
+        print('Quit is succesful')
+        break
 
+    elif user_choice == '1':
+        user_dir = input('Наберите полный путь папки:')
+        hw05_easy.change_dir(user_dir)
 
+    elif user_choice == '2':
+        dir_name = os.getcwd()
+        hw05_easy.list_dir(dir_name)
 
+    elif user_choice == '3':
+        dir_name = input('Наберите полный путь папки: ')
+        hw05_easy.rmv_dir(dir_name)
 
-
-
-
-
-
-
+    elif user_choice == '4':
+        dir_name = input('Наберите полный путь папки: ')
+        hw05_easy.make_dir(dir_name)
